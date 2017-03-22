d5f2bdccd6cdd4a9eb4713aa712433cee78dfb20
David Reid <dreid@dreid.org>
2017-03-22 12:15:56 -0700

    Fix up the other paths.

--- extra/run-dogstatsd.sh
+++ extra/run-dogstatsd.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+DD_AGENT_ROOT=""
+
 if [[ $DATADOG_API_KEY ]]; then
-  sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  sed -i -e "s/^.*api_key:.*$/api_key: ${DATADOG_API_KEY}/" $DD_AGENT_ROOT/opt/datadog-agent/agent/datadog.conf
 else
   echo "DATADOG_API_KEY environment variable not set. Run: heroku config:add DATADOG_API_KEY=<your API key>"
   exit 1
 fi
 
 if [[ $HEROKU_APP_NAME ]]; then
-  sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  sed -i -e "s/^.*hostname:.*$/hostname: ${HEROKU_APP_NAME}/" $DD_AGENT_ROOT/opt/datadog-agent/agent/datadog.conf
 else
   echo "HEROKU_APP_NAME environment variable not set. Run: heroku apps:info|grep ===|cut -d' ' -f2"
   exit 1
 fi
 
 if [[ $DATADOG_HISTOGRAM_PERCENTILES ]]; then
-  sed -i -e "s/^.*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" /app/.apt/opt/datadog-agent/agent/datadog.conf
+  sed -i -e "s/^.*histogram_percentiles:.*$/histogram_percentiles: ${DATADOG_HISTOGRAM_PERCENTILES}/" $DD_AGENT_ROOT/opt/datadog-agent/agent/datadog.conf
 fi
 
 (
@@ -25,7 +27,7 @@ fi
     # Unset other PYTHONPATH/PYTHONHOME variables before we start
     unset PYTHONHOME PYTHONPATH
     # Load our library path first when starting up
-    export LD_LIBRARY_PATH=/app/.apt/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$DD_AGENT_ROOT/opt/datadog-agent/embedded/lib:$LD_LIBRARY_PATH
     mkdir -p /tmp/logs/datadog
     exec /etc/init.d/datadog-agent start
   fi
